dc616279e199530a3be955dfdb840a634384717e
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-23 22:02:13 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName] = useState("");
+  const [password,setPassword] = useState("");
+
+  const [error,setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(name && password){
+      setError("");
+    }else{
+      setError("Both username and password are required");
+    }
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form>
+          <label for="name">Username: </label>
+          <input type="text" placeholder="Name" onChange = {(e)=>setName(e.target.value)}/><br/>
+          <label for="password">Password: </label>
+          <input type="password" placeholder="password" onChange = {(e)=>setPassword(e.target.value)}/><br/>
+          {(name === "" || password === "") && 
+            <p style={{color:'red'}}>{error}</p>}
+          <button  onClick={handleSubmit}>Submit</button>
+        </form>
+
     </div>
   )
 }
